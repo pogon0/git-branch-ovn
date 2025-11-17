3e6445f6804ff9f71e5813beaf3dfd970df7c357
mikael-johnsson <mikaeljohanjohnsson@gmail.com>
2025-11-17 13:39:08 +0100

    add fake type for development

--- src/services/serviceBase.ts
+++ src/services/serviceBase.ts
@@ -1,5 +1,13 @@
-export const getData = async (pokemon: string) => {
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-  const data = await response.json();
+type Pokemon = {
+  name: string;
+  weight: number;
+  isDefault: boolean;
+};
+
+export const getData = async (searchTerm: string) => {
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${searchTerm}`
+  );
+  const data: Pokemon = await response.json();
   return data;
 };
